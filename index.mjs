0f4b4452ebe0a6da3c55c81f641345b13186ad4c
Marcel Klehr <mklehr@gmx.net>
2024-12-10 09:11:27 +0100

    fix(regex): Don't include final slashes

--- index.mjs
+++ index.mjs
@@ -4,17 +4,18 @@ import core from '@actions/core'
 (async () => {
   // `website` input defined in action metadata file
   const website = core.getInput('website');
-  console.log(`Scraping ${website}!`);
+  console.log(`Scraping ${website}`);
   const res = await fetch(website)
   const html = await res.text()
   const users = Array.from(
     new Set(
       Array.from(
-        html.matchAll(/<a [^>]*?href=["']https:\/\/github.com\/([^?]*?)["']/g)
+        html.matchAll(/<a [^>]*?href=["']https:\/\/github.com\/([^?\/]*?)\/?["']/g)
       )
       .map(([_, handle]) => handle)
     )
   )
+  console.log('Found', users)
   core.setOutput("users", users.join(','));
 })().catch(error => {
   core.setFailed(error.message);
